7c04f7f99e0d74ce7361fb84e7c95bb1b72b3c5f
juliswer <julianswer36@gmail.com>
2021-09-30 20:54:17 -0300

    uploading new files

---
+++ screens/ImageScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const ImageScreen = () => {
+    return (
+        <View>
+            <Text>Image Screen</Text>
+        </View>
+    )
+}
+
+export default ImageScreen
